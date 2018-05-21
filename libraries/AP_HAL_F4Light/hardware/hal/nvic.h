c737f66bdc436fa7d17e397d00fb2d67ccef057c
night-ghost <night_ghost@ykoctpa.ru>
2018-05-21 15:14:53 +0500

    HAL_F4Light: removed dependency from SPL's stm32f4xx_nvic driver

--- libraries/AP_HAL_F4Light/hardware/hal/nvic.h
+++ libraries/AP_HAL_F4Light/hardware/hal/nvic.h
@@ -6,6 +6,16 @@
 extern "C"{
 #endif
 
+
+#define AIRCR_VECTKEY_MASK    ((uint32_t)0x05FA0000)
+#define NVIC_PriorityGroup_0         ((uint32_t)0x700) // 0 bits for pre-emption priority 4 bits for subpriority 
+#define NVIC_PriorityGroup_1         ((uint32_t)0x600) // 1 bits for pre-emption priority 3 bits for subpriority 
+#define NVIC_PriorityGroup_2         ((uint32_t)0x500) // 2 bits for pre-emption priority 2 bits for subpriority 
+#define NVIC_PriorityGroup_3         ((uint32_t)0x400) // 3 bits for pre-emption priority 1 bits for subpriority 
+#define NVIC_PriorityGroup_4         ((uint32_t)0x300) // 4 bits for pre-emption priority 0 bits for subpriority 
+
+
+
 void enable_nvic_irq(uint8_t irq, uint8_t prio);
 
 
